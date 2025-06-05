5a311ab44d81aa887dd8a5044698c9b8759fe74a
pratham sharma <prathamsharma0916@gmail.com>
2025-06-05 16:14:43 +0530

    migrattion

--- tweeter/tweeter/views.py
+++ tweeter/tweeter/views.py
@@ -9,12 +9,17 @@ from django.core.management import call_command
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
+from django.http import HttpResponse
+from django.core.management import call_command
+
 def run_migrations(request):
     try:
-        call_command('migrate')
-        return HttpResponse("✅ Migrations applied successfully.")
+        call_command('makemigrations')  # Create migration files
+        call_command('migrate')         # Apply them to the DB
+        return HttpResponse("✅ Makemigrations and Migrate applied successfully.")
     except Exception as e:
         return HttpResponse(f"❌ Error: {e}")
+
     
 def run_collectstatic(request):
     try:
