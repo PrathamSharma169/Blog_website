57167d2dca34b2c038ad02990640ca827c9edefc
pratham sharma <prathamsharma0916@gmail.com>
2025-08-15 23:10:36 +0530

    .

--- tweeter/tweeter/views.py
+++ tweeter/tweeter/views.py
@@ -14,7 +14,7 @@ from django.core.management import call_command
 
 def run_migrations(request):
     try:
-        # call_command('makemigrations')  # Create migration files
+        call_command('makemigrations')  # Create migration files
         # call_command('migrate', 'tweet', fake=True) # Apply them to the DB
         call_command('migrate')
         return HttpResponse("✅ Makemigrations and Migrate applied successfully.")
