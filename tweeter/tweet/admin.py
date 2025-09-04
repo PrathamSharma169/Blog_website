f9106ddc4f2f9593c17ae5a47018fb759b29e2b7
pratham sharma <prathamsharma0916@gmail.com>
2025-09-05 01:09:32 +0530

    admin page

--- tweeter/tweet/admin.py
+++ tweeter/tweet/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Tweet
+from .models import Comment, Collaborations
 # Register your models here.
 admin.site.register(Tweet)
+admin.site.register(Comment)
+admin.site.register(Collaborations)
